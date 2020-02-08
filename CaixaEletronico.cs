ccc90ca9bae665a8da6c89a15eef7229ca8ec3d3
pedro-ss <pedross.tool@gmail.com>
2020-02-08 14:35:38 -0300

    adicionando métodos de apresentação

--- CaixaEletronico.cs
+++ CaixaEletronico.cs
@@ -4,9 +4,33 @@ namespace caixaeletronico
 {
     class CaixaEletronico
     {
+        public void menu(){
+            Console.WriteLine("Seja bem vindo ao caixa do seu banco.");
+            Console.WriteLine("Favor selecionar uma das opções abaixo:");
+            Console.WriteLine("1 - Criar conta");
+            Console.WriteLine("2 - Fazer login em conta");
+            Console.WriteLine("3 - Sair");
+        }
+
+        public void criarConta(){
+
+        }
+
+        public Boolean login(){
+            Boolean logged = false;
+            
+            return logged;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int option;
+            CaixaEletronico caixa = new CaixaEletronico();
+            caixa.menu();
+            option = int.Parse(Console.ReadLine());
+
+            if(option > 0){
+                Console.WriteLine("Opção válida!");
+            }
         }
     }
 }
