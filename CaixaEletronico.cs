555470174a6512a83cf6801fe2e183743926a5a0
pedro-ss <pedross.tool@gmail.com>
2020-02-08 14:04:09 -0300

    Criando projeto do tipo console - dotnet new console

---
+++ CaixaEletronico.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace caixaeletronico
+{
+    class CaixaEletronico
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
